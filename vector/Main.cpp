7178fec3453b1af33ec72454f0d0a3ba4908fba2
iceshow53 <iceshow53@gmail.com>
2023-05-17 11:16:58 +0900

    초기 커밋.

---
+++ vector/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int* Numbers = nullptr;
+int Length = 0;
+
+void push(int n)
+{
+	if (Numbers == nullptr)
+	{
+		Numbers = new int;
+		*Numbers = n;
+		++Length;
+		return;
+	}
+
+	int* Temp = new int[Length + 1];
+	
+	for (int i = 0; i < Length; ++i)
+		Temp[i] = Numbers[i];
+
+	Temp[Length] = n;
+	Numbers = Temp;
+	++Length;
+}
+
+int main(void)
+{
+	//push(10);
+	//push(20);
+	//push(30);
+	//push(40);
+
+	for (int i = 0; i < Length; ++i)
+	{
+		cout << Numbers[i] << endl;
+	}
+
+	vector<int> vecList;
+
+	for (int i = 0; i < 128; ++i)
+	{
+		vecList.push_back(i * 10 + 10);
+		cout << vecList[i] << endl;
+		cout << vecList.capacity() << endl << endl;
+	}
+
+	return 0;
+}
